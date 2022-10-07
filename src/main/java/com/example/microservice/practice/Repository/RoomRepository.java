fed10c0baae5efec5e407ecb5d51ea346245e8d9
Divyadharshini25 <115237227+Divyadharshini25@users.noreply.github.com>
2022-10-07 18:26:08 +0530

    Delete src directory

--- src/main/java/com/example/microservice/practice/Repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservice.practice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.microservice.practice.Bean.HotelRoom;
-
-public interface RoomRepository extends JpaRepository<HotelRoom,String> {
-
-}
